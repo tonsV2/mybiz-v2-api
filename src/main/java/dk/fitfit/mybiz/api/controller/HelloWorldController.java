31ec070624dbd3dc4b9287455f2c24a60ce836b5
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-03-19 17:41:22 +0100

    Added angular client.

--- src/main/java/dk/fitfit/mybiz/api/controller/HelloWorldController.java
+++ src/main/java/dk/fitfit/mybiz/api/controller/HelloWorldController.java
@@ -3,18 +3,21 @@ package dk.fitfit.mybiz.api.controller;
 import dk.fitfit.mybiz.business.domain.Expense;
 import dk.fitfit.mybiz.business.service.ExpenseServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class HelloWorldController {
 	@Autowired
 	ExpenseServiceInterface expenseService;
 
-	@RequestMapping("/hello")
-	public Expense helloWorld() {
-		return expenseService.findOne(123);
+	@RequestMapping("/expense/{id}")
+	public Expense helloWorld(@PathVariable Long id) {
+		return expenseService.findOne(id);
 	}
 }
