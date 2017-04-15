26e8a20bfcb1650820754057f98ff2c16538628b
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-16 01:48:31 +0200

    Added init method.

--- src/main/java/dk/fitfit/mybiz/api/controller/ExpenseController.java
+++ src/main/java/dk/fitfit/mybiz/api/controller/ExpenseController.java
@@ -2,6 +2,7 @@ package dk.fitfit.mybiz.api.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import dk.fitfit.mybiz.api.resource.ExpenseResource;
 import dk.fitfit.mybiz.api.resource.ExpenseResources;
 import dk.fitfit.mybiz.api.resource.assembler.ExpenseResourceAssembler;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -72,7 +75,28 @@ public class ExpenseController {
 		expense.setDescription(resource.getDescription());
 		expense.setPrice(resource.getPrice());
 		expense.setAmount(resource.getAmount());
+		expenseService.save(expense);
 		return new ResponseEntity<>(assembler.toResource(expense), HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/init", method = GET)
+	public ExpenseResources init() {
+		Expense expense = new Expense();
+		expense.setName("Shalala name...");
+		expense.setDescription("some description");
+		expense.setAmount(666);
+		expense.setPrice(12);
+		Expense saved = expenseService.save(expense);
+
+		Expense expense1 = new Expense();
+		expense1.setName("Other expense name...");
+		expense1.setDescription("Some other description");
+		expense1.setAmount(1212);
+		expense1.setPrice(6);
+		Expense saved1 = expenseService.save(expense1);
+
+		List<Expense> expenses = Lists.newArrayList(saved, saved1);
+		return assembler.toResources(expenses);
 	}
 
 	private void log(final String format, final ExpenseResource resource) {
