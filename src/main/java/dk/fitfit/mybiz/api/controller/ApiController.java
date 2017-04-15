f0dbd7fa584c8305a9d35f543e10a0109436c373
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-16 01:13:41 +0200

    Removed logging from api controller.

--- src/main/java/dk/fitfit/mybiz/api/controller/ApiController.java
+++ src/main/java/dk/fitfit/mybiz/api/controller/ApiController.java
@@ -1,8 +1,6 @@
 package dk.fitfit.mybiz.api.controller;
 
 import dk.fitfit.mybiz.api.resource.ApiResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/api")
 public class ApiController {
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
-
 	@RequestMapping("/v0.1")
 	public ApiResource getApi() {
-		log.info("getApi()");
 		return new ApiResource();
 	}
 }
